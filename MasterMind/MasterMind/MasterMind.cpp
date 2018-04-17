80c6b815198159196a5a5a4a0700cc5ea28ee119
patrenaud <1810057@bart.ca>
2018-04-17 13:53:47 -0400

    Commit Principal

--- MasterMind/MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind/MasterMind.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <random>
 
+
+std::random_device depart;
+std::mt19937 hasard(depart());
+std::uniform_int_distribution<> d6(1, 6);
 
 int main()
 {
-    return 0;
+
+	std::string Grid[10][4] = { { " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " },
+								{ " "," "," "," " } };
+
+	std::string Colors[6] = { "J","R","B","M","C","V" };
+	std::string CPU[4] = {Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)]};
+	
+
+	return 0;
 }
 
