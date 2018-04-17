1d1077a297962c01d7e49e309695947952383b01
patrenaud <1810057@bart.ca>
2018-04-17 14:57:47 -0400

    Commit ShowGrid

--- MasterMind/MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind/MasterMind.cpp
@@ -6,29 +6,63 @@
 #include <string>
 #include <random>
 
+void Instructions();
+void ShowGrid(std::vector<std::vector<std::string>> &Grid, std::vector<std::string> CPU);
 
 std::random_device depart;
 std::mt19937 hasard(depart());
-std::uniform_int_distribution<> d6(1, 6);
+std::uniform_int_distribution<> d6(0, 5);
 
 int main()
 {
 
-	std::string Grid[10][4] = { { " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " },
-								{ " "," "," "," " } };
+	std::vector<std::vector<std::string>> Grid = {  { " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " },
+													{ " "," "," "," " } };
 
-	std::string Colors[6] = { "J","R","B","M","C","V" };
-	std::string CPU[4] = {Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)]};
-	
+	std::vector<std::string> Colors = { "J","R","B","M","C","V" };
+	std::vector<std::string> CPU = {Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)]};
+
+	Instructions();
+	ShowGrid(Grid, CPU);
 
 	return 0;
 }
 
+void Instructions()
+{
+	std::cout
+		<< "Les règles sont simples, vous devez entrer 4 couleurs et trouver la" << std::endl
+		<< "combinaison gagnante. Les billes noires indiquent les bonnes réponses" << std::endl
+		<< "et les billes blanches les réponses mal placées. Bonne chance!" << std::endl
+		<< "---------------------------------------------------------------------" << std::endl
+		<< "Vous devez mettre un espace entre vos lettres en MAJUSCULE" << std::endl
+		<< "J = Jaune / R = Rouge / B = Bleu / M = Mauve / O = Orange / V = Vert" << std::endl;
+}
+
+void ShowGrid(std::vector<std::vector<std::string>> &Grid, std::vector<std::string> CPU )
+{
+	int round = 0;
+	int Win = 0;
+
+	do 
+	{
+		std::cout << "Entrez 4 couleurs entre J, R, B, M, O, V : " << std::endl;
+		std::cin >> Grid[round][0] >> Grid[round][1] >> Grid[round][2] >> Grid[round][3];
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << "| "<< Grid[i][0] << " | " << Grid[i][1] << " | " << Grid[i][2] << " | " << Grid[i][3] << " |" << std::endl;
+			std::cout << "+---+---+---+---+" << std::endl;
+		}
+
+
+	} while (Win == 1 || round == 10);
+
+}
