86df828e1d4e6478ed2ec99798910720340dd2be
patrenaud <1810057@bart.ca>
2018-04-26 18:33:48 -0400

    Final Commit

--- MasterMind/MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind/MasterMind.cpp
@@ -8,6 +8,9 @@
 
 void Instructions();
 void ShowGrid(std::vector<std::vector<std::string>> &Grid, std::vector<std::string> CPU);
+void CheckWin(std::vector<std::vector<std::string>> Grid, std::vector<std::string> CPU, int round, int& Win);
+int CheckWinWhite(std::vector<std::string> &CopieCPU, std::vector<std::vector<std::string>> Grid, int round);
+int CheckWinBlack(std::vector<std::string> &CopieCPU, std::vector<std::vector<std::string>> Grid, int round);
 
 std::random_device depart;
 std::mt19937 hasard(depart());
@@ -16,7 +19,7 @@ std::uniform_int_distribution<> d6(0, 5);
 int main()
 {
 
-	std::vector<std::vector<std::string>> Grid = {  { " "," "," "," " },
+	std::vector<std::vector<std::string>> Grid = { { " "," "," "," " },
 													{ " "," "," "," " },
 													{ " "," "," "," " },
 													{ " "," "," "," " },
@@ -27,8 +30,8 @@ int main()
 													{ " "," "," "," " },
 													{ " "," "," "," " } };
 
-	std::vector<std::string> Colors = { "J","R","B","M","C","V" };
-	std::vector<std::string> CPU = {Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)]};
+	std::vector<std::string> Colors = { "J","R","B","M","O","V" };
+	std::vector<std::string> CPU = { Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)], Colors[d6(hasard)] };
 
 	Instructions();
 	ShowGrid(Grid, CPU);
@@ -47,24 +50,90 @@ void Instructions()
 		<< "J = Jaune / R = Rouge / B = Bleu / M = Mauve / O = Orange / V = Vert" << std::endl;
 }
 
-void ShowGrid(std::vector<std::vector<std::string>> &Grid, std::vector<std::string> CPU )
+void ShowGrid(std::vector<std::vector<std::string>> &Grid, std::vector<std::string> CPU)
 {
 	int round = 0;
 	int Win = 0;
 
-	do 
+	do
 	{
 		std::cout << "Entrez 4 couleurs entre J, R, B, M, O, V : " << std::endl;
 		std::cin >> Grid[round][0] >> Grid[round][1] >> Grid[round][2] >> Grid[round][3];
 		for (int i = 0; i < 10; i++)
 		{
-			std::cout << "| "<< Grid[i][0] << " | " << Grid[i][1] << " | " << Grid[i][2] << " | " << Grid[i][3] << " |" << std::endl;
+			std::cout << "| " << Grid[i][0] << " | " << Grid[i][1] << " | " << Grid[i][2] << " | " << Grid[i][3] << " |" << std::endl;
 			std::cout << "+---+---+---+---+" << std::endl;
 		}
+
+		CheckWin(Grid, CPU, round, Win);
+		round++;
 
 		if (round == 10)
 		{
 			std::cout << "Vous avez perdu, la solution était: " << CPU[0] << CPU[1] << CPU[2] << CPU[3] << std::endl;
 		}
-	} while (Win == 1 || round == 10);
+	} while (Win != 1 || round != 10);
+}
+
+void CheckWin(std::vector<std::vector<std::string>> Grid, std::vector<std::string> CPU, int round, int& Win)
+{
+	std::vector<std::string> CopieCPU = CPU;
+
+	std::cout << "BILLES NOIR - Nombre de bonne réponses (couleurs bien placées) : " << CheckWinBlack(CopieCPU, Grid, round) << std::endl;
+	std::cout << "BILLES BLANCHE - Nombre de réponses valides mais mal positionnées : " << CheckWinWhite(CopieCPU, Grid, round) << std::endl;
+
+	if (Grid[round][0] == CPU[0] && Grid[round][1] == CPU[1] && Grid[round][2] == CPU[2] && Grid[round][3] == CPU[3])
+	{
+		std::cout << "You Win" << std::endl;
+		Win = 1;
+	}
+}
+
+int CheckWinBlack(std::vector<std::string> &CopieCPU, std::vector<std::vector<std::string>> Grid, int round)
+{
+	int noir = 0;
+
+	for (int i = 0; i <= 3; i++)
+	{
+		if (Grid[round][i] == CopieCPU[i])
+		{
+			noir++;
+			CopieCPU[i] = ' ';
+		}
+	}
+	return noir;
+}
+
+
+int CheckWinWhite(std::vector<std::string> &CopieCPU, std::vector<std::vector<std::string>> Grid, int round)
+{
+	int blanc = 0;
+
+	for (int i = 0; i <= 3; i++)
+	{
+		if (Grid[round][i] != CopieCPU[i])
+		{
+			if (Grid[round][i] == CopieCPU[0])
+			{
+				blanc++;
+				CopieCPU[0] = ' ';
+			}
+			else if (Grid[round][i] == CopieCPU[1])
+			{
+				blanc++;
+				CopieCPU[1] = ' ';
+			}
+			else if (Grid[round][i] == CopieCPU[2])
+			{
+				blanc++;
+				CopieCPU[2] = ' ';
+			}
+			else if (Grid[round][i] == CopieCPU[3])
+			{
+				blanc++;
+				CopieCPU[3] = ' ';
+			}
+		}
+	}
+	return blanc;
 }
